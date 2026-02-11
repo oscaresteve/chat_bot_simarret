75922557386f728b5cf56da8b7439398b2de2eed
oscaresteve <oscarestevea24@gmail.com>
2026-02-11 09:39:40 +0100

    first commit

---
+++ src/main/java/edu/alumno/oscaresteve/chat_bot_simarret/ChatBotSimarretApplication.java
@@ -0,0 +1,13 @@
+package edu.alumno.oscaresteve.chat_bot_simarret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotSimarretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotSimarretApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/alumno/oscaresteve/chat_bot_simarret/ChatBotSimarretApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alumno.oscaresteve.chat_bot_simarret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotSimarretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
